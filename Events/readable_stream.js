eb713825cbd9be2c6192c083bf80ea5bcb7bffe5
moises.correa <correa.moises3@gmail.com>
2018-05-11 09:26:05 -0500

    Ejercicio con streams

---
+++ Events/readable_stream.js
@@ -0,0 +1,8 @@
+const request = require('request');
+const fs = require('fs');
+
+const req = request('http://www.google.com');
+req.pipe(fs.createWriteStream('./page.json'));
+req.on('data', chunk => console.log('chuck', chunk));
+//req.on('response', res => console.log('res', res.statusCode));
+req.on('end', () => console.log('Finish'));
