f39e08c2c5a7020a2a407bdb1e3b8a287bb9e94b
moises.correa <correa.moises3@gmail.com>
2018-05-11 10:28:16 -0500

    Ejecicio con Buffers

---
+++ Buffers/buffer_exercise.js
@@ -0,0 +1,8 @@
+const b = new Buffer('Hello');
+
+console.log(b.toString());
+console.log(b.toString('base64'));
+
+const v = new Buffer('world').toString('base64');
+
+console.log(b.toString('utf-8',0,3));
